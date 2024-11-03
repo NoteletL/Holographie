2916965d8c095d38abc94ed3c13d92f87982b04a
Fryhw <81508836+Fryhw@users.noreply.github.com>
2024-11-03 15:37:19 +0100

    Delete lanceTrain.sh

--- lanceTrain.sh
+++
@@ -1,5 +0,0 @@
-
-
-rep=$(pwd)
-
-python main_holo_tsf.py --checkpoint_dir $rep/holography/checkpoints/$1 --sample_dir $rep/holography/eval_samples/ --params "chosenIteration=${2}, phase=train" --save_dir "./data1/"
